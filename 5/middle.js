e7182dce7c4d619bace278c4ef3c1e160dc1bc18
MatiasGonzalez23 <niceinsane700@gmail.com>
2021-11-07 21:52:41 -0300

    final

--- 5/middle.js
+++ 5/middle.js
@@ -11,7 +11,15 @@ const { Server } = require('engine.io');
 ///////////////////////////////////////////////////////
 
 var server = http.createServer(function(req, res){
-    fs.readFile('./pagina.html',function(error,data){
+    var objectourl=url.parse(req.url);
+    var camino= 'static'+objectourl.pathname;
+    if(camino=='static/')
+       camino='static/pagina.html';
+       
+     
+
+
+    fs.readFile(camino,function(error,data){
         res.writeHead(200,{'Content-Type':'text/html'});
         res.end(data,'utf-8');
     })
